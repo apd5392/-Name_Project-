22f6db34322e7fef0a42b9fa87d27bbc79a34f36
apd5392 <adoumbia.pas@gmail.com>
2022-07-27 18:38:52 -0400

    Villains updated

---
+++ client/my-app/src/components/VillainCard.jsx
@@ -0,0 +1,20 @@
+export default function VillainCard(props) {
+    console.log(props)
+  if (!props) {
+    return <h1>Loading please wait</h1>;
+  } else {
+    return (
+      <div>
+        <img src={props.image}/>
+        <h2>{props.name}</h2>
+        <h3 className="h3Age">Age: {props.age}</h3>
+        <h3 className="h3heading">Other Names:</h3>
+        <h3>{props.otherName}</h3>
+        <h3 className="h3heading">Anime Series:</h3>
+        <h3>{props.anime}</h3>
+        <h3 className="h3heading">Groups: </h3>
+        <h3>{props.organizations}</h3>
+      </div>
+    );
+  }
+}
--- client/my-app/src/pages/Villains.js
+++ client/my-app/src/pages/Villains.js
@@ -1,10 +1,41 @@
-import '../Villains.css';
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import VillainCard from '../components/VillainCard'
+import "../Villains.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import VillainCard from "../components/VillainCard";
 
 const Villains = () => {
+  const [villains, setVillains] = useState([]);
+  useEffect(() => {
+    const fetchVillains = async () => {
+      try {
+        const res = await axios.get(`http://localhost:3001/villains`);
+        setVillains(res.data.villains);
+        console.log(res.data.villains);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchVillains();
+  }, []);
 
-}
-
-export default Villains
+  if (!villains) {
+    return <h1>Loading Please Wait!</h1>;
+  } else {
+    return (
+      <div className="villainDiv">
+        {villains.map((villain) => (
+          <VillainCard
+            className="villainCardDiv"
+            name={villain.name}
+            image={villain.image}
+            otherName={villain.otherName}
+            anime={villain.anime}
+            age={villain.age}
+            organizations={villain.organizations}
+          />
+        ))}
+      </div>
+    );
+  }
+};
+export default Villains;
